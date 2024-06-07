ad016e6452f31465c6a68e211116ffa68c4810b6
Lê Phạm Xuân Duyên <117836247+lephamxuanduyen@users.noreply.github.com>
2024-06-07 11:19:08 +0700

    init source

---
+++ src/test/java/chapter1/DriverTest.java
@@ -0,0 +1,35 @@
+package chapter1;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DriverTest {
+
+    @Test
+    public static void ChromeTest(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://www.selenium.dev/documentation/");
+        String actualTitle = chromeDriver.getTitle();
+        String expectedTitle = "The Selenium Browser Automation Project | Selenium";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+        chromeDriver.close();
+    }
+
+    @Test
+    public static void FirefoxTest(){
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver firefoxDriver = new FirefoxDriver();
+        firefoxDriver.get("https://www.selenium.dev/documentation/");
+        String actualTitle = firefoxDriver.getTitle();
+        String expectedTitle = "The Selenium Browser Automation Project | Selenium";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+        firefoxDriver.close();
+    }
+
+}
